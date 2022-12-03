f17ee254292775b5a18ce9462a48bee681a6de7b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-12-03 19:05:22 -0300

    Add bio location and website field to user 💅💅💅

--- users/models.py
+++ users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    location = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="We are big on real names around here, so people know who is who.",
+    )
+    bio = models.TextField(
+        blank=True,
+        help_text="Brief description for your profile. URLs are hyperlinked.",
+    )
+
+    personal_website = models.URLField(
+        blank=True, help_text="Your home page, blog or company site."
+    )
