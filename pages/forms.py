72c3077da8eb6520a4b81ba934c83b53f00f81cc
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-12-13 15:18:23 -0300

    Add new form for sending feedback"

--- pages/forms.py
+++ pages/forms.py
@@ -7,6 +7,40 @@ from django.core.mail import send_mail
 logger = logging.getLogger(__name__)
 
 
+class FeedbackForm(forms.Form):
+    subject = "Feedback Message"
+    email = forms.EmailField(
+        required=True,
+        min_length=10,
+        widget=forms.TextInput(attrs={"placeholder": "Email"}),
+    )
+    message = forms.CharField(
+        min_length=20,
+        max_length=1000,
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                "cols": 80,
+                "rows": 5,
+                "placeholder": "Send us your feedback or report an issue. Please provide as much info as possible. Thank you.",
+            }
+        ),
+    )
+
+    def send_mail(self):
+        from_email = self.cleaned_data["email"]
+        message = "Email: {email}\n\n{message}".format(**self.cleaned_data)
+
+        logger.info("sending feedback...")
+        send_mail(
+            subject=self.subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=[settings.DEFAULT_TO_EMAIL],
+            fail_silently=False,
+        )
+
+
 class ContactForm(forms.Form):
     name = forms.CharField(
         max_length=100,
