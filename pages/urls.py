d60521ebb17139ef0984706a4a4c842238516ea7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-12-17 15:18:05 -0300

    Move public profile wild card match to the end

--- pages/urls.py
+++ pages/urls.py
@@ -13,11 +13,11 @@ urlpatterns = [
     path("contact/", views.ContactPageView.as_view(), name="contact"),
     path("feedback/", views.FeedbackPageView.as_view(), name="feedback"),
     path("favicon.ico", views.favicon),
-    path("<str:slug>/", views.PublicProfilePageView.as_view(), name="profile"),
     path("base/", views.BasePageView.as_view(), name="base"),  # TODO: Veer remove this!
     path(
         "base-fullscreen/",
         views.BaseFullScreenPageView.as_view(),
         name="base-fullscreen",
     ),  # TODO: Veer remove this!
+    path("<str:slug>/", views.PublicProfilePageView.as_view(), name="profile"),
 ]
