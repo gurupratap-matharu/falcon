8d728a361024e01652c3e360aa9e9c05fc3e7aec
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-01-25 13:19:22 -0300

    Implement unit tests for companies

---
+++ companies/tests/test_models.py
@@ -0,0 +1,65 @@
+from django.db import IntegrityError
+from django.test import TestCase
+
+from companies.factories import CompanyFactory
+from companies.models import Company
+
+
+class CompanyModelTests(TestCase):
+    """Test suite for the Company Model"""
+
+    def setUp(self):
+        self.company = CompanyFactory()
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.company), self.company.name)
+
+    def test_get_absolute_url(self):
+        expected_url = f"/companies/{self.company.slug.lower()}/"
+        self.assertEqual(self.company.get_absolute_url(), expected_url)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(self.company._meta.verbose_name_plural), "companies")
+
+    def test_company_model_creation_is_accurate(self):
+        company_from_db = Company.objects.first()
+
+        self.assertEqual(Company.objects.count(), 1)
+        self.assertEqual(company_from_db.name, self.company.name)
+        self.assertEqual(company_from_db.slug, self.company.slug)
+
+    def test_company_name_max_length(self):
+        company = Company.objects.first()
+        max_length = company._meta.get_field("name").max_length
+
+        self.assertEqual(max_length, 200)
+
+    def test_company_slug_max_length(self):
+        company = Company.objects.first()
+        max_length = company._meta.get_field("slug").max_length
+
+        self.assertEqual(max_length, 300)
+
+    def test_all_companies_have_unique_slugs(self):
+        _ = Company.objects.create(name="Andesmar", slug="andesmar")
+
+        with self.assertRaises(IntegrityError):
+            Company.objects.create(name="Andes a Mar", slug="andesmar")
+
+    def test_all_companies_are_ordered_correctly(self):
+        Company.objects.all().delete()
+
+        c_1 = Company.objects.create(name="cata", slug="cata")
+        c_2 = Company.objects.create(name="Bariloche Travels", slug="bari-travels")
+        c_3 = Company.objects.create(name="Andesmar", slug="andesmar")
+
+        companies = Company.objects.all()
+
+        self.assertEqual(companies[0], c_3)
+        self.assertEqual(companies[1], c_2)
+        self.assertEqual(companies[2], c_1)
+
+        company = companies[0]
+        ordering = company._meta.ordering[0]
+
+        self.assertEqual(ordering, "name")
--- companies/tests/test_views.py
+++ companies/tests/test_views.py
@@ -4,14 +4,15 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from companies.factories import CompanyFactory
-from companies.views import CompanyListView
+from companies.views import CompanyDetailView, CompanyListView
+from users.factories import UserFactory
 
 
 class CompanyListTests(TestCase):
     """Test suite for company list view"""
 
     def setUp(self):
-        self.url = reverse("companies:company-list")
+        self.url = reverse("companies:company_list")
         self.template_name = "companies/company_list.html"
         self.response = self.client.get(self.url)
 
@@ -36,3 +37,37 @@ class CompanyListTests(TestCase):
         self.assertEqual(len(response.context["companies"]), 2)
         self.assertContains(response, company_1.name)
         self.assertContains(response, company_2.name)
+
+
+class CompanyDetailViewTests(TestCase):
+    def setUp(self):
+        self.company = CompanyFactory()
+        self.url = self.company.get_absolute_url()
+        self.template_name = "companies/company_detail.html"
+        self.response = self.client.get(self.url)
+
+    def test_company_detail_view_works_correctly(self):
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(self.response, self.template_name)
+        self.assertContains(self.response, self.company.name)
+        self.assertContains(self.response, self.company.description)
+        self.assertNotContains(self.response, "Hi I should not be on this page")
+
+    def test_company_detail_url_resolves_correct_view(self):
+        view = resolve(self.url)
+        self.assertEqual(view.func.__name__, CompanyDetailView.as_view().__name__)
+
+    def test_anonymous_user_can_access_company_detail_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+        self.assertContains(response, self.company.name)
+
+    def test_logged_in_user_can_access_company_detail_view(self):
+        user = UserFactory()
+        self.client.force_login(user)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+        self.assertContains(response, self.company.name)
