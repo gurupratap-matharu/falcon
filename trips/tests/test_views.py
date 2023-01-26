54ca82aef3ec754d6a98383f35a24fbd2def0033
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-01-25 23:23:05 -0300

    Unit tests for TripDetailView 💆🏻‍♀️

--- trips/tests/test_views.py
+++ trips/tests/test_views.py
@@ -3,7 +3,9 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from trips.views import TripListView
+from trips.factories import TripFactory
+from trips.views import TripDetailView, TripListView
+from users.factories import UserFactory
 
 
 class TripListViewTests(TestCase):
@@ -25,3 +27,40 @@ class TripListViewTests(TestCase):
     def test_trip_list_url_resolve_triplistview(self):
         view = resolve(self.url)
         self.assertEqual(view.func.__name__, TripListView.as_view().__name__)
+
+
+class TripDetailViewTests(TestCase):
+    """Test Suite for Trip DetailView"""
+
+    def setUp(self):
+        self.trip = TripFactory()
+        self.url = self.trip.get_absolute_url()
+        self.template_name = "trips/trip_detail.html"
+        self.response = self.client.get(self.url)
+
+    def test_trip_detail_view_works_correctly(self):
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(self.response, self.template_name)
+        self.assertContains(self.response, self.trip.origin)
+        self.assertContains(self.response, self.trip.destination)
+        self.assertContains(self.response, self.trip.price)
+        self.assertNotContains(self.response, "Hi I should not be on this page")
+
+    def test_trip_detail_url_resolves_correct_view(self):
+        view = resolve(self.url)
+        self.assertEqual(view.func.__name__, TripDetailView.as_view().__name__)
+
+    def test_anonymous_user_can_access_trip_detail_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+        self.assertContains(response, self.trip.price)
+
+    def test_logged_in_user_can_access_trip_detail_view(self):
+        user = UserFactory()
+        self.client.force_login(user)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, self.template_name)
+        self.assertContains(response, self.trip.price)
