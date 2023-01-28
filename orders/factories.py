31f35d93d1595d270db7f83e821ce77f760780fd
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-01-28 12:09:25 -0300

    Fix bug to remove null choices

--- orders/factories.py
+++ orders/factories.py
@@ -1,9 +1,12 @@
 import factory
 from factory import fuzzy
+from faker import Faker
 
 from trips.factories import TripFactory
 
 from .models import Order, OrderItem, Passenger
+
+fake = Faker()
 
 
 class OrderFactory(factory.django.DjangoModelFactory):
@@ -48,7 +51,7 @@ class PassengerFactory(factory.django.DjangoModelFactory):
 
     order_item = factory.SubFactory(OrderItemFactory)
     document_type = fuzzy.FuzzyChoice(
-        Passenger.DOCUMENT_TYPE_CHOICES, getter=lambda c: c[0]
+        Passenger.DOCUMENT_TYPE_CHOICES[1:], getter=lambda c: c[0]
     )
     document_number = factory.Faker("ssn")
     nationality = factory.Faker("country_code")
@@ -56,5 +59,7 @@ class PassengerFactory(factory.django.DjangoModelFactory):
     last_name = factory.Faker("last_name_nonbinary")
     gender = factory.Faker("random_element", elements=["M", "F"])
     birth_date = factory.Faker("date_of_birth")
-    phone_number = factory.Faker("country_calling_code")
+    phone_number = factory.LazyAttribute(
+        lambda _: (fake.country_calling_code() + fake.phone_number())[:14]
+    )
     seat_number = factory.Faker("random_int", min=1, max=60)
