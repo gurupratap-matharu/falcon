da57e9a9c434e2dfb6b4c7d1ea82158fff635e62
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-14 19:10:07 -0300

    Add admin view to download order as pdf

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,21 @@
 import logging
 from typing import Any, Dict
 
+from django.conf import settings
+from django.contrib.admin.views.decorators import staff_member_required
 from django.forms import modelformset_factory
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
+
+import weasyprint
 
 from cart.cart import Cart
 
 from .forms import OrderForm, PassengerForm
-from .models import OrderItem, Passenger
+from .models import Order, OrderItem, Passenger
 from .services import order_created
 
 logger = logging.getLogger(__name__)
@@ -123,19 +129,14 @@ class OrderCreateView(CreateView):
             return super().form_invalid(form)
 
 
-"""
-OrderConfirmedView()
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string("orders/order_pdf.html", {"order": order})
 
-This view is triggered when we get a webhook notification that an order is paid.
-Generally I think this should be instantaneous in most cases before we show the
-success url page. But we have to try it out.
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = f"attachment; filename=order_{order.id}.pdf"
 
-            # Basically veer we need to do the following things here
-            # 1. extract the order + order_items object from the DB
-            # 2. confirm the seats for each order item with their respective trips
-            # 3. when successful generate a pdf of the ticket
-            # 4. shoot an email to the payer with pdf attached
-            # 5. route to final success kind of page to download the ticket + ical
-            # 6. prompt to book return ticket if this was one way else show some recommendations(not part of mvp)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=None)
 
-"""
+    return response
