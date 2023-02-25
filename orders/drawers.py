e2934d75001bb2cd37a244a818a41b2e116acbf3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-25 20:21:14 -0300

    Remove blocker note as script is fixed 👩🏻‍🔧

--- orders/drawers.py
+++ orders/drawers.py
@@ -1,16 +1,7 @@
 """
 Exposes helper methods to draw order data in pdf and other formats.
-
-Blocker Note:
-    Veer the weasy print module is slowing down our linter on vscode considerably
-    and we have no quick fix at the moment. The immediate solution is to move all
-    weasy print write_pdf() calls in a standalone script to keep the linting and
-    formatting of other files fast.
-
-    Not sure what causes this. But its just the write_pdf() method of weasyprint.
-    I am unable to find a way to ignore it somehow. vscode stucks on analysing files
-    for several seconds.
 """
+
 import logging
 from timeit import default_timer as timer
 
