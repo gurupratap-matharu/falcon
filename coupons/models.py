a56b9ce6c4073109dbdfddd25316298b5553c776
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-26 17:50:28 -0300

    Implement instance methods to verify validity ✅

--- coupons/models.py
+++ coupons/models.py
@@ -1,11 +1,17 @@
+import logging
+
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+logger = logging.getLogger(__name__)
+
 
 class Coupon(models.Model):
+    """One time use coupons to apply a discount to a cart"""
+
     code = models.CharField(
         max_length=50,
         unique=True,
@@ -43,6 +49,28 @@ class Coupon(models.Model):
                 {"valid_to": _("Valid to date cannot be less than Valid from date")},
                 code="invalid",
             )
+
+    def is_valid(self):
+        """
+        Determine if the coupon itself is valid or not.
+        The criteria is:
+            - coupon should not already be redeemed (i.e. be active)
+            - valid_from < now < valid_to
+        """
+
+        now = timezone.now()
+        return self.active and (self.valid_from < now < self.valid_to)
+
+    def deactivate(self):
+        """Used when a coupon is redeemed by a user so that it cannot be used again"""
+
+        if not self.is_valid():
+            raise ValidationError("coupon is already invalid 🤷‍♀️")
+
+        logger.info("deactivating coupon(💣):%s..." % self)
+
+        self.active = False
+        self.save()
 
     def __str__(self):
         return self.code
