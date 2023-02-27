26a481eef4669c95a567c6923701b1d136240948
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-02-27 12:43:09 -0300

    Add CSS classes to coupon form widget

--- coupons/forms.py
+++ coupons/forms.py
@@ -4,4 +4,6 @@ from django import forms
 class CouponApplyForm(forms.Form):
     """Simple form to allow a user to apply a coupon to the cart"""
 
-    code = forms.CharField()
+    code = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Code", "class": "form-control"})
+    )
