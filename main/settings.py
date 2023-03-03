41f988251402b3ecbe853a3af40230373a0c303b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-03-03 16:30:21 -0300

    Upgrade shell imports

--- main/settings.py
+++ main/settings.py
@@ -302,6 +302,7 @@ LOGGING = {
 
 # django extensions
 SHELL_PLUS_IMPORTS = [
+    "from companies.factories import CompanyFactory",
     "from trips.factories import TripFactory, TripTomorrowFactory, SeatFactory, SeatWithPassengerFactory",
     "from orders.factories import OrderFactory, OrderItemFactory, PassengerFactory",
     "from coupons.factories import CouponFactory",
