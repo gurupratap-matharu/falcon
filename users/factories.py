7a23d69612b484c0368d8ba0890a53ae34441fbf
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-03-06 11:34:23 -0300

    Implement Group and company owner factories

--- users/factories.py
+++ users/factories.py
@@ -1,9 +1,28 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 import factory
 
 
+class GroupFactory(factory.django.DjangoModelFactory):
+    """Generic factory to create groups in a sequence"""
+
+    class Meta:
+        model = Group
+        django_get_or_create = ("name",)
+
+    name = factory.Sequence(lambda n: "Group_{0}".format(n))
+
+
+class OperatorGroupFactory(GroupFactory):
+    """Our custom group that allows CRUD permissions on Trip | Location | Seat models"""
+
+    name = "Operators"
+
+
 class UserFactory(factory.django.DjangoModelFactory):
+    """Public app users with normal privileges"""
+
     class Meta:
         model = get_user_model()
         django_get_or_create = ("email",)
@@ -21,10 +40,33 @@ class UserFactory(factory.django.DjangoModelFactory):
 
 
 class StaffuserFactory(UserFactory):
+    """Internal users to Falcon with staff privileges"""
+
     is_staff = True
     username = factory.Sequence(lambda n: "staffuser%d" % n)
 
 
 class SuperuserFactory(StaffuserFactory):
+    """This is me."""
+
     is_superuser = True
     username = factory.Sequence(lambda n: "superuser%d" % n)
+
+
+class CompanyOwnerFactory(UserFactory):
+    """
+    A subset of our app users which are owners | staff of the companies they create.
+
+    We need to add them to the `Operators` group so that they can do CRUD on
+    trips | locations | seats models that they themselves create.
+    """
+
+    @factory.post_generation
+    def groups(self, create, extracted, **kwargs):
+
+        if not create or not extracted:
+            # Simple build, or nothing to add, do nothing.
+            return
+
+        # Add the iterable of groups using bulk addition
+        self.groups.add(*extracted)  # type:ignore
