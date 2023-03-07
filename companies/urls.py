3c356c7f1729eb1d4a411b94e9dfb31a8ef202b4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-03-07 15:09:45 -0300

    simplify url conf for companies'

--- companies/urls.py
+++ companies/urls.py
@@ -1,19 +1,16 @@
 from django.urls import include, path
 
+from trips.urls import trip_crud_patterns
+
 from . import views
 
 app_name = "companies"
 
-company_admin_patterns = [
-    path("", views.CompanyDashboardView.as_view(), name="dashboard"),
-    path("trips/", views.ManageTripListView.as_view(), name="manage_trip_list"),
-    path("trips/create/", views.TripCreateView.as_view(), name="trip_create"),
-    path("trips/<uuid:id>/update/", views.TripUpdateView.as_view(), name="trip_update"),
-    path("trips/<uuid:id>/delete/", views.TripDeleteView.as_view(), name="trip_delete"),
-]
-
 urlpatterns = [
+    # Public Views
     path("", views.CompanyListView.as_view(), name="company_list"),
     path("<slug:slug>/", views.CompanyDetailView.as_view(), name="company_detail"),
-    path("<slug:slug>/admin/", include(company_admin_patterns)),
+    # Company Admin Views
+    path("<slug:slug>/admin/", views.CompanyDashboardView.as_view(), name="dashboard"),
+    path("<slug:slug>/admin/trips/", include(trip_crud_patterns)),
 ]
