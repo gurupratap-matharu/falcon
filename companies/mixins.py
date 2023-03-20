6920730e62bea56ad71567850911a7004517a7f2
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-03-19 22:12:57 -0300

    Awesome mixin for cutting edge security 👮‍♀️

---
+++ companies/mixins.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+    UserPassesTestMixin,
+)
+
+from companies.models import Company
+
+
+class OwnerMixin(LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin):
+    """
+    Handy Mixin to allow company owners to do CRUD on only their own objects.
+    """
+
+    company = None
+    permission_required = "trips.view_trip"
+
+    def test_func(self):
+        self.company = Company.objects.select_related("owner").get(
+            slug=self.kwargs["slug"]
+        )
+        user = self.request.user
+        return user.is_superuser or self.company.owner == user
