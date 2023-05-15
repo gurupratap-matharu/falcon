04d6967bd248601055d2885c499480962464290a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-05-15 15:35:21 -0300

    Add edge cases incase of missing fields in forms

--- trips/forms.py
+++ trips/forms.py
@@ -28,6 +28,9 @@ class TripSearchForm:
     This class does just that.
     """
 
+    VALID_TRIP_TYPES = ("round_trip", "one_way")
+    VALID_NUM_PASSENGERS = 10
+
     def __init__(self, data: QueryDict | dict[str, list[str]]):
         self.data = data or {}
 
@@ -38,6 +41,8 @@ class TripSearchForm:
         self.clean_destination()
         self.clean_departure()
         self.clean_return()
+        self.clean_num_passengers()
+        self.clean_trip_type()
 
     def clean_origin(self):
         """Only allow origins present in our database"""
@@ -45,8 +50,10 @@ class TripSearchForm:
         logger.info("cleaning origin...")
 
         origin = self.data.get("origin")
+
         if isinstance(origin, list):
             origin = origin[0]
+
         return get_object_or_404(Location, name__iexact=origin)
 
     def clean_destination(self):
@@ -55,46 +62,94 @@ class TripSearchForm:
         logger.info("cleaning destination...")
 
         destination = self.data.get("destination")
+
         if isinstance(destination, list):
             destination = destination[0]
+
         return get_object_or_404(Location, name__iexact=destination)
 
     def clean_departure(self):
         """
-        Make sure departure is not in the past
+        Make sure departure is not in the past and is supplied
         """
 
         logger.info("cleaning departure...")
 
         departure = self.data.get("departure")
+
+        if not departure:
+            raise ValidationError(_("Departure date is invalid"))
+
         if isinstance(departure, list):
             departure = departure[0]
 
         self.departure_date = datetime.strptime(departure, "%d-%m-%Y").date()
 
         if self.departure_date < datetime.today().date():
-            raise ValidationError("Departure cannot be in the past!")
+            raise ValidationError(_("Departure cannot be in the past!"))
 
     def clean_return(self):
         """
         Don't allow return date to be less than departure date
+        Note: Return date is optional in our form.
         """
 
         logger.info("cleaning return...")
 
+        today = datetime.today().date()
+        departure_date = self.departure_date
         return_date = self.data.get("return")
-        if isinstance(return_date, list):
-            return_date = return_date[0]
 
         if not return_date:
             return
 
-        self.return_date = datetime.strptime(return_date, "%d-%m-%Y").date()
+        if isinstance(return_date, list):
+            return_date = return_date[0]
 
-        if self.return_date < self.departure_date:
+        return_date = datetime.strptime(return_date, "%d-%m-%Y").date()
+
+        if (return_date < departure_date) or (return_date < today):
             raise ValidationError(
-                _("Return date cannot be less than departure date"),
+                _("Return date is invalid"),
             )
+
+    def clean_trip_type(self):
+        """
+        Only allow trip types to be in self.VALID_TRIP_TYPES
+        """
+
+        logger.info("cleaning trip type...")
+
+        trip_type = self.data.get("trip_type")
+
+        if not trip_type:
+            raise ValidationError(_("Trip type is not valid"))
+
+        if isinstance(trip_type, list):
+            trip_type = trip_type[0]
+
+        if trip_type not in self.VALID_TRIP_TYPES:  # type:ignore
+            raise ValidationError(_("Trip type is not valid"))
+
+    def clean_num_passengers(self):
+        """
+        Only allow num of passengers to be <= self.VALID_NUM_PASSENGERS
+        """
+
+        logger.info("cleaning num passengers...")
+
+        num_of_passengers = self.data.get("num_of_passengers")
+
+        if not num_of_passengers:
+            raise ValidationError(_("Number of passengers is not valid"))
+
+        if isinstance(num_of_passengers, list):
+            num_of_passengers = num_of_passengers[0]
+
+        num_of_passengers = int(num_of_passengers)  # type:ignore
+
+        if num_of_passengers < 1 or num_of_passengers > self.VALID_NUM_PASSENGERS:
+            raise ValidationError(_("Number of passengers is not valid"))
 
     def __repr__(self):
         return json.dumps(self.data)
