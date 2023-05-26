8ff12fd41ad33fd4b4fb7daa793d1a4f0fe733de
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-05-26 14:47:29 -0300

    Sitemap for companies on our platform

---
+++ companies/sitemaps.py
@@ -0,0 +1,14 @@
+from django.contrib.sitemaps import Sitemap
+
+from .models import Company
+
+
+class CompanySitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Company.objects.all()
+
+    def lastmod(self, obj):
+        return obj.updated_at
