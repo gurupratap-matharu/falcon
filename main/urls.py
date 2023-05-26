cb0e87a6c5683d7910e219c8847ecd0d787e4d50
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-05-26 14:47:49 -0300

    Route in sitemap endpoint with urlconf 🔗

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+
+from companies.sitemaps import CompanySitemap
+
+sitemaps = {"companies": CompanySitemap}
+
 
 urlpatterns = [
     # Django admin
@@ -34,6 +40,12 @@ urlpatterns = [
     path("payments/", include("payments.urls")),
     path("companies/", include("companies.urls")),
     path("coupons/", include("coupons.urls")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
     path("", include("pages.urls")),
 ]
 
