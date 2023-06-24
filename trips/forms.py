ad01f5aa305499b54c0a6a7fe5600053ac20a760
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-06-24 13:54:25 -0300

    Introduce recurrence form ⏰ with unit tests

--- trips/forms.py
+++ trips/forms.py
@@ -4,9 +4,13 @@ from datetime import datetime
 
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.http.request import QueryDict
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+from dateutil import rrule
 
 from trips.models import Location
 
@@ -156,6 +160,14 @@ class TripSearchForm:
 
 
 class TripCreateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.errors:
+            attrs = self[field].field.widget.attrs
+            attrs.setdefault("class", "")
+            attrs["class"] += " is-invalid"
+
     class Meta:
         model = Trip
         fields = (
@@ -172,16 +184,10 @@ class TripCreateForm(forms.ModelForm):
 
         widgets = {
             "name": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "required": "required",
-                }
+                attrs={"class": "form-control", "required": "required"}
             ),
             "origin": forms.Select(
-                attrs={
-                    "class": "form-select",
-                    "required": "required",
-                },
+                attrs={"class": "form-select", "required": "required"}
             ),
             "destination": forms.Select(
                 attrs={"class": "form-select", "required": "required"}
@@ -192,11 +198,8 @@ class TripCreateForm(forms.ModelForm):
             "arrival": forms.DateTimeInput(
                 format="%Y-%m-%d %H:%M:%S", attrs={"class": "form-control arrival"}
             ),
-            "price": forms.TextInput(
-                attrs={
-                    "class": "form-select",
-                    "required": "required",
-                }
+            "price": forms.NumberInput(
+                attrs={"class": "form-control", "required": "required"}
             ),
             "status": forms.Select(
                 attrs={"class": "form-select", "required": "required"}
@@ -206,10 +209,149 @@ class TripCreateForm(forms.ModelForm):
             ),
             "description": forms.Textarea(
                 attrs={
-                    "class": "form-select",
+                    "class": "form-control",
                     "placeholder": "(Optional)",
                     "cols": 80,
                     "rows": 5,
                 }
             ),
         }
+
+
+class RecurrenceForm(forms.Form):
+    """
+    Form to create multiple instances of an event based on dateutil rrules.
+    """
+
+    FREQUENCY_CHOICES = [
+        (None, "----------"),
+        (rrule.HOURLY, _("Every Hour")),
+        (rrule.DAILY, _("Every Day")),
+        (rrule.WEEKLY, _("Every Week")),
+        (rrule.MONTHLY, _("Every Month")),
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.errors:
+            logger.info("field: %s has error" % field)
+            attrs = self[field].field.widget.attrs
+            attrs.setdefault("class", "")
+            attrs["class"] += " is-invalid"
+
+    dtstart = forms.DateTimeField(
+        label=_("Starts"),
+        initial=datetime.now,
+        error_messages={"required": _("Please enter start date")},
+        widget=forms.DateTimeInput(attrs={"class": "form-control"}),
+    )
+
+    freq = forms.ChoiceField(
+        label=_("Repeats"),
+        help_text=_("Frequency of the event (required)"),
+        choices=FREQUENCY_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+
+    until = forms.DateTimeField(
+        label=_("End Repeat"),
+        help_text=_("Provide either End repeat or Count."),
+        required=False,
+        widget=forms.DateTimeInput(attrs={"class": "form-control"}),
+    )
+
+    count = forms.IntegerField(
+        label=_("Count"),
+        help_text=_(
+            "(Optional) End repeat after these many occurrences (between 0-1000). "
+            "Provide either End repeat or Count."
+        ),
+        required=False,
+        validators=[MinValueValidator(0), MaxValueValidator(1000)],
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+    )
+
+    interval = forms.IntegerField(
+        label=_("Interval"),
+        initial=1,
+        help_text=_(
+            "(Optional) Time between consecutive occurrences. "
+            "Ex for daily repeats 1: Every day, 2: Every other day, 3: every third day and so on..."
+        ),
+        required=False,
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+    )
+
+    def _clean_dt(self, dt):
+        """Make sure datetime is not in the past"""
+
+        if dt and dt.date() < timezone.localdate():
+            raise ValidationError(
+                _("datetime (%(dt)s) cannot be in the past!"),
+                code="invalid",
+                params={"dt": dt.strftime("%d-%m-%Y %H:%M:%S")},
+            )
+
+        return dt
+
+    def clean_dtstart(self):
+        """Make sure start date is not in the past"""
+
+        dtstart = self.cleaned_data["dtstart"]
+        return self._clean_dt(dt=dtstart)
+
+    def clean_until(self):
+        """Make sure end date is not in the past"""
+
+        until = self.cleaned_data["until"]
+        return self._clean_dt(dt=until)
+
+    def clean_freq(self):
+        freq = self.cleaned_data["freq"]
+
+        if not freq:
+            return
+        return int(freq)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        dtstart = cleaned_data.get("dtstart")
+        until = cleaned_data.get("until")
+        count = cleaned_data.get("count")
+
+        # End date cannot be less than start date
+        if dtstart and until and until < dtstart:
+            msg = _("End time cannot be less than Start time.")
+            self.add_error("until", msg)
+
+        # Either until or count is needed
+        if not until and not count:
+            msg = _("Provide either End repeat or Count.")
+            self.add_error("until", msg)
+            self.add_error("count", msg)
+
+    def save(self):
+        logger.info("saving recurrence form...")
+
+        occurrences = self.build_occurrence_timestamps()
+        logger.info("Total occurrences: %s" % occurrences.count())
+        logger.info("occurrences: %s" % occurrences)
+
+        return occurrences
+
+    def build_occurrence_timestamps(self):
+        """
+        Use the cleaned data as rrule params for the dateutil library and
+        build a list of datetime objects which represent future occurrence timestamps.
+        """
+
+        cd = self.cleaned_data
+        params = {k: v for k, v in cd.items() if v is not None}
+
+        logger.info("params:%s" % params)
+        logger.info("building occurrence timestamps...")
+
+        return rrule.rrule(**params)
--- trips/tests/test_forms.py
+++ trips/tests/test_forms.py
@@ -3,9 +3,12 @@ from datetime import datetime, timedelta
 from django.core.exceptions import ValidationError
 from django.http import Http404
 from django.test import TestCase
+from django.utils import timezone
+
+from dateutil import rrule
 
 from trips.factories import LocationFactory
-from trips.forms import TripSearchForm
+from trips.forms import RecurrenceForm, TripSearchForm
 
 
 class TripSearchFormTests(TestCase):
@@ -253,3 +256,256 @@ class TripSearchFormTests(TestCase):
 
         with self.assertRaises(ValidationError):
             form_2.validate()
+
+
+class RecurrenceFormTests(TestCase):
+    """
+    Test suite to validate the recurrence form.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.now = timezone.now()
+
+        cls.yesterday = cls.now - timedelta(days=1)
+        cls.tomorrow = cls.now + timedelta(days=1)
+        cls.day_after = cls.now + timedelta(days=2)
+        cls.next_week = cls.now + timedelta(days=7)
+        cls.next_month = cls.now + timedelta(days=31)
+        cls.next_year = cls.now + timedelta(days=365)
+
+    def test_recurrence_form_is_valid_for_valid_data(self):
+        data = {"dtstart": self.tomorrow, "until": self.day_after, "freq": rrule.DAILY}
+        form = RecurrenceForm(data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_recurrence_form_is_invalid_missing_start_date(self):
+        data = {"until": self.day_after, "freq": rrule.MONTHLY}
+        form = RecurrenceForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {"dtstart": ["Please enter start date"]})
+
+    def test_recurrence_form_is_invalid_for_past_start_date(self):
+        ts = self.yesterday.strftime("%d-%m-%Y %H:%M:%S")
+        data = {
+            "dtstart": self.yesterday,
+            "until": self.day_after,
+            "freq": rrule.HOURLY,
+        }
+        form = RecurrenceForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors,
+            {"dtstart": [f"datetime ({ts}) cannot be in the past!"]},
+        )
+
+    def test_recurrence_form_is_invalid_for_past_end_date(self):
+        ts = self.yesterday.strftime("%d-%m-%Y %H:%M:%S")
+        data = {"dtstart": self.tomorrow, "until": self.yesterday, "freq": rrule.WEEKLY}
+        form = RecurrenceForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["until"],
+            [
+                f"datetime ({ts}) cannot be in the past!",
+                "Provide either End repeat or Count.",
+            ],
+        )
+
+    def test_recurrence_form_is_invalid_if_both_count_and_until_are_missing(self):
+        data = {"dtstart": self.tomorrow, "freq": rrule.DAILY}
+        form = RecurrenceForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors,
+            {
+                "until": ["Provide either End repeat or Count."],
+                "count": ["Provide either End repeat or Count."],
+            },
+        )
+
+    def test_recurrence_form_is_invalid_if_end_date_is_less_than_start_date(self):
+        data = {"dtstart": self.day_after, "until": self.tomorrow, "freq": rrule.DAILY}
+        form = RecurrenceForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors, {"until": ["End time cannot be less than Start time."]}
+        )
+
+    def test_recurrence_form_is_invalid_missing_frequency(self):
+        data = {"dtstart": self.tomorrow, "until": self.day_after}
+        form = RecurrenceForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {"freq": ["This field is required."]})
+
+    # Test valid creation
+    # Hourly
+    def test_hourly_recurrence_until_date(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "until": self.day_after,
+            "freq": rrule.HOURLY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_hourly_recurrence_with_count(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "count": 5,
+            "freq": rrule.HOURLY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_hourly_recurrence_with_interval(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "until": self.day_after,
+            "freq": rrule.HOURLY,
+            "interval": 3,  # every three hours
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    # Daily
+    def test_daily_recurrence_until_date(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "until": self.next_week,
+            "freq": rrule.DAILY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_daily_recurrence_with_count(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "count": 5,
+            "freq": rrule.DAILY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_daily_recurrence_with_interval(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "until": self.next_month,
+            "freq": rrule.DAILY,
+            "interval": 2,  # every other day
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    # Weekly
+    def test_weekly_recurrence_until_date(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "until": self.next_month,
+            "freq": rrule.WEEKLY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_weekly_recurrence_with_count(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "count": 25,
+            "freq": rrule.WEEKLY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_weekly_recurrence_with_interval(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "count": 25,
+            "freq": rrule.WEEKLY,
+            "interval": 2,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    # Monthly
+    def test_monthly_recurrence_until_date(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "until": self.next_year,
+            "freq": rrule.MONTHLY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_monthly_recurrence_with_count(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "count": 24,
+            "freq": rrule.MONTHLY,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
+
+    def test_monthly_recurrence_with_interval(self):
+        params = {
+            "dtstart": self.tomorrow,
+            "count": 36,
+            "freq": rrule.MONTHLY,
+            "interval": 2,
+        }
+
+        form = RecurrenceForm(params)
+        actual = list(form.build_occurrence_timestamps())
+        expected = list(rrule.rrule(**params))
+
+        self.assertEqual(actual, expected)
