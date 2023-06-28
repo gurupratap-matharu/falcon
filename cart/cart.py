f50d1fae663002fc86172be85163ce57d8ef948a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-06-28 15:19:54 -0300

    Simplify initialization of cart

--- cart/cart.py
+++ cart/cart.py
@@ -22,33 +22,28 @@ class Cart:
         logger.info("initializing cart...")
 
         self.session = request.session
-
-        cart = self.session.get(settings.CART_SESSION_ID)
-
-        if not cart:
-            # save an empty cart in the session
-            cart = self.session[settings.CART_SESSION_ID] = {}
-
-        self.cart = cart
+        self.cart = self.session.setdefault(settings.CART_SESSION_ID, {})
         self.coupon_id = self.session.get("coupon_id")
 
     @property
     def coupon(self):
-        if self.coupon_id:
-            try:
-                return Coupon.objects.get(id=self.coupon_id)
-            except Coupon.DoesNotExist:
-                pass
-
-        return None
+        return Coupon.objects.filter(id=self.coupon_id).first()
 
     def get_discount(self):
         """Calculate the discount applied on the total cart by a valid coupon (if any)"""
 
-        if self.coupon:
-            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        if not self.coupon:
+            return Decimal(0)
+        return (self.coupon.discount / Decimal(100)) * self.get_total_price()
 
-        return Decimal(0)
+    def get_total_price(self):
+        """
+        Calculate the total price across all trips and their quantities
+        """
+
+        return sum(
+            Decimal(item["price"]) * item["quantity"] for item in self.cart.values()
+        )
 
     def get_total_price_after_discount(self):
         """Calculate the final price after applying coupon discount (if any)"""
@@ -105,15 +100,6 @@ class Cart:
 
         del self.session[settings.CART_SESSION_ID]
         self.save()
-
-    def get_total_price(self):
-        """
-        Calculate the total price across all trips and their quantities
-        """
-
-        return sum(
-            Decimal(item["price"]) * item["quantity"] for item in self.cart.values()
-        )
 
     def to_dict(self):
         """Builds a dict which is JSON serializable"""
