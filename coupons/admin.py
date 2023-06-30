ed70ba68060711b8b876e6bb3a9952fd313108b0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-06-30 15:50:35 -0300

    Supercharge the coupon admin interface

--- coupons/admin.py
+++ coupons/admin.py
@@ -5,6 +5,14 @@ from .models import Coupon
 
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ("code", "valid_from", "valid_to", "discount", "active")
+    list_display = ("code", "discount", "valid", "redeemed")
     list_filter = ("active", "valid_from", "valid_to")
     search_fields = ("code",)
+
+    @admin.display(boolean=True, description="Is Valid?")
+    def valid(self, obj):
+        return obj.is_valid()
+
+    @admin.display(boolean=True, description="Is Redeemed?")
+    def redeemed(self, obj):
+        return not obj.active
