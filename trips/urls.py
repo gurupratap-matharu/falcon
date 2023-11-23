8f696afef93870a2d582a4068cca52c3c94d2150
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-11-23 11:28:41 -0300

    Remove redundant location urls from trips namespace

--- trips/urls.py
+++ trips/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 
-from .views import (
-    LocationDetailView,
-    LocationListView,
-    TripDetailView,
-    TripListView,
-    TripSearchView,
-)
+from .views import TripDetailView, TripListView, TripSearchView
 
 app_name = "trips"
 
@@ -16,8 +10,4 @@ urlpatterns = [
     path("", TripListView.as_view(), name="trip-list"),
     path("<uuid:id>/<slug:slug>/", TripDetailView.as_view(), name="trip_detail"),
     path("search/", TripSearchView.as_view(), name="trip-search"),
-    path(
-        "locations/<slug:slug>/", LocationDetailView.as_view(), name="location-detail"
-    ),
-    path("locations/", LocationListView.as_view(), name="location-list"),
 ]
