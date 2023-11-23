a7eb5eba2f1a749ea4b7ec7168b1ff9bc01a522f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-11-23 13:42:09 -0300

    Add possible url patterns for locations

--- trips/location_urls.py
+++ trips/location_urls.py
@@ -7,4 +7,7 @@ app_name = "locations"
 urlpatterns = [
     path("<slug:slug>/", LocationDetailView.as_view(), name="location-detail"),
     path("", LocationListView.as_view(), name="location-list"),
+    # nice to have
+    # "/terminals?latitude=49.0000&longitude=2.55" <- find terminals closest to this lat lon
+    # "<slug:slug>/destinations/?max=2" <- show all destinations (locations) from this location
 ]
