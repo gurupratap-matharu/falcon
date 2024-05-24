1ebd0fb1af389df2c93d41bcdcf601f12b19fd43
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2024-05-24 14:39:44 -0300

    Add default shell imports

--- main/settings.py
+++ main/settings.py
@@ -326,13 +326,14 @@ LOGGING = {
 
 # django extensions
 SHELL_PLUS_IMPORTS = [
-    "import json",
+    "import json, random",
     "from datetime import datetime, timedelta",
     "from users.factories import UserFactory, StaffuserFactory, SuperuserFactory, CompanyOwnerFactory",
     "from companies.factories import CompanyFactory, SeatChartFactory",
     "from trips.factories import LocationFactory",
     "from trips.factories import TripFactory, TripTomorrowFactory, SeatFactory, SeatWithPassengerFactory",
     "from orders.factories import OrderFactory, OrderItemFactory, PassengerFactory",
+    "from orders.services import order_confirmed",
     "from coupons.factories import CouponFactory",
 ]
 
