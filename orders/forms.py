4854241900d3a8c67805d8f4fd124068839b0906
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2024-06-17 22:55:50 -0300

    Improve accessibility in templates

--- orders/forms.py
+++ orders/forms.py
@@ -19,6 +19,7 @@ class OrderForm(forms.ModelForm):
             attrs = self[field].field.widget.attrs
             attrs.setdefault("class", "")
             attrs["class"] += " is-invalid"
+            pdb.set_trace()
 
     class Meta:
         model = Order
