78782a5370fa334c152f193f68daa4cba946a48e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2024-06-24 16:09:08 -0300

    Advance with home page search form UI tests

--- main/tests/test_functional.py
+++ main/tests/test_functional.py
@@ -1,7 +1,12 @@
+from datetime import timedelta
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.utils import timezone
 
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
@@ -16,13 +21,14 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUpClass(cls) -> None:
         super().setUpClass()
         cls.browser = webdriver.Firefox()
+        # cls.browser.implicitly_wait(2)
 
     @classmethod
     def tearDownClass(cls) -> None:
         cls.browser.quit()
         super().tearDownClass()
 
-    def test_can_search_for_a_trip(self):
+    def test_home_page_search_form(self):
         # Vicky has heard about a cool new bus ticket booking app. She goes to check out its homepage
         self.browser.get(f"{self.live_server_url}")
 
@@ -30,35 +36,68 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn("Kpiola", self.browser.title)
         self.assertIn("Book Bus Tickets", self.browser.title)
 
-        # She is invited to enter the origin, destination and journey date right away
+        # She notices the origin input box and the placeholder
         origin = self.browser.find_element(By.NAME, "origin")
-        destination = self.browser.find_element(By.NAME, "destination")
+        self.assertEqual(origin.get_attribute("placeholder"), "Leaving from")
+        self.assertEqual(origin.get_attribute("value"), "")
 
-        self.assertEqual(origin.get_attribute("placeholder"), "Search origin")
-        self.assertEqual(destination.get_attribute("placeholder"), "Search destination")
-
+        # She doesn't see any autocomplete dangling below the input
         origin_ac = self.browser.find_element(By.ID, "autoComplete_list_1")
+        self.assertFalse(origin_ac.is_displayed())
 
-        destination_ac = self.browser.find_element(By.ID, "autoComplete_list_2")
-
-        # She types Bue (for buenos aires) in the origin box
+        # She types in `Bue`` to search for `Buenos Aires` and sees the autocomplete popup
         origin.send_keys("bue")
+        self.assertTrue(origin_ac.is_displayed)
 
-        # She should see the auto complete results
-        self.assertTrue(origin_ac.is_displayed())
+        # She selects Buenos Aires from the dropdown autocomplete list
+        ActionChains(self.browser).move_to_element(origin_ac).click().perform()
+        self.assertEqual(origin.get_attribute("value"), "Buenos Aires")
 
-        # She types Men (for mendoza) in the destination box
+        # Destination input
+        # She notices the destination input box and the placeholder
+        destination = self.browser.find_element(By.NAME, "destination")
+        self.assertEqual(destination.get_attribute("placeholder"), "Going to")
+        self.assertEqual(destination.get_attribute("value"), "")
+
+        # She doesn't see any autocomplete dangling below the input
+        destination_ac = self.browser.find_element(By.ID, "autoComplete_list_2")
+        self.assertFalse(destination_ac.is_displayed())
+
+        # She types in `Men` to search for `Mendoza` and sees the autocomplete popup
         destination.send_keys("men")
+        self.assertTrue(destination_ac.is_displayed)
 
-        # She should see the auto complete results
-        self.assertTrue(destination_ac.is_displayed())
+        # She selects Mendoza from the dropdown autocomplete list
+        ActionChains(self.browser).move_to_element(destination_ac).click().perform()
+        # self.assertEqual(destination.get_attribute("value"), "Mendoza") # <-- flaky
 
-        # self.fail("Please finish the test!")
+        # She observes the departure date input is prepopulated with tomorrow's date
+        departure = self.browser.find_element(By.ID, "departure")
+        tomorrow = (timezone.now() + timedelta(days=1)).strftime("%d-%m-%Y")
+        self.assertEqual(departure.get_attribute("value"), tomorrow)
 
-        # When she hits 'search' the page updates with relevant results showing buses available
+        # ?? Not sure how to text flatpickr for departure
 
-        # Vicky wonders about...
-        # how much luggage she can carry
-        # whether the bus journey provides meals on board
-        # how is the cancellation policy?
-        # Then she notes that the site suggest a FAQ where most of her urgent queries are responded clearly
+        # She observes the return date input and notices that its blank
+        return_date = self.browser.find_element(By.ID, "return")
+        self.assertEqual(return_date.get_attribute("value"), "")
+
+        # ?? Again not sure how to test flatpickr for return date input
+
+        # Trip Type
+        # Vicky then notices the trip type option and see `Round Trip` as selected
+        trip_type = Select(self.browser.find_element(By.ID, "trip_type"))
+        self.assertEqual(trip_type.first_selected_option.text, "Round trip")
+
+        # She clicks on it and sees that there are only two options
+        self.assertEqual(len(trip_type.options), 2)
+
+        # Num of Passenger
+        # Vicky then moves her attention to the num of passengers options
+        num_of_pass = Select(self.browser.find_element(By.ID, "num_of_passengers"))
+        self.assertEqual(num_of_pass.first_selected_option.text, "1")
+
+        # She clicks on the select and see that there are upto 5 options
+        self.assertEqual(len(num_of_pass.options), 5)
+
+        # Being satisfied that all inputs are corrected she finally clicks the search button
