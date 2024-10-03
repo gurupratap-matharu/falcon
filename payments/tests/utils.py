355b8865964d0ef89ca16833c7eea59f28d739cd
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2024-10-03 15:50:44 -0300

    Add patch for Modo Api call

---
+++ payments/tests/utils.py
@@ -0,0 +1,14 @@
+import uuid
+
+
+class ModoMock:
+    def __init__(self, order):
+        print("mock payment intent called 🔥")
+        pass
+
+    def json(self):
+        return {
+            "id": str(uuid.uuid4()),
+            "qr": "https:://qr.abc.modo.com",
+            "deeplink": "https://modo.deeplink.com",
+        }
