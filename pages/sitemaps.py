504ac9b5859ce5c6ccf5982bedc411179b6a4b0c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2024-10-10 13:11:53 -0300

    Update urls in pages sitemaps

--- pages/sitemaps.py
+++ pages/sitemaps.py
@@ -7,7 +7,16 @@ class StaticViewSitemap(sitemaps.Sitemap):
     changefreq = "daily"
 
     def items(self):
-        return ["home", "about", "terms", "privacy", "contact", "feedback"]
+        return [
+            "home",
+            "about",
+            "terms",
+            "privacy",
+            "contact",
+            "feedback",
+            "sitemap",
+            "help",
+        ]
 
     def location(self, item):
         return reverse(f"pages:{item}")
