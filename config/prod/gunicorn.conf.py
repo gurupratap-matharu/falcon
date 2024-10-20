c83f62787607dbc1001cf8a985cf06f3d0c2c40f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2024-10-20 12:46:05 -0300

    Supercharge gunicorn config

--- config/prod/gunicorn.conf.py
+++ config/prod/gunicorn.conf.py
@@ -2,16 +2,19 @@
 # https://docs.gunicorn.org/en/stable/configure.html#configuration-file
 # https://docs.gunicorn.org/en/stable/settings.html
 
-import multiprocessing  # noqa
+# import multiprocessing
+import gunicorn
 
-# restart workers after so many requests with some variability
-max_requests = 1000
+# Replace gunicorn server http header to avoid leaking info to attackers
+gunicorn.SERVER = ""
+
+# restart workers every 1200-1250 requests
+max_requests = 1200
 max_requests_jitter = 50
 
 
 # Bind to a unix socket (created by systemd)
 bind = "unix:/run/falcon-gunicorn.sock"
-
 
 # Define the number of workers
 # workers = multiprocessing.cpu_count() * 2 + 1
@@ -27,12 +30,16 @@ error_log_format = '%(h)s %(l)s %(u)s "%(r)s" %(s)s'
 
 log_file = "-"
 
-
 # Whether to send Django output to the error log
 capture_output = True
-
 
 # How verbose the Gunicorn error logs should be
 loglevel = "info"
 
 enable_stdio_inheritance = True
+
+# Load app-prefork to save memory and worker startup time
+preload_app = True
+
+# Timeout after 25 secs
+timeout = 25
